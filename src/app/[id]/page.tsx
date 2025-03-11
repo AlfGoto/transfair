45a6947db8f1d289c27bdf387e8c8a8f7e434893
Alf <alfgoto@gmail.com>
2025-03-11 20:59:16 +0100

    fix: return notfound() removed

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -8,26 +8,26 @@ export interface FileItem {
 }
 
 async function getFiles(id: string): Promise<FileItem[]> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_TRANSFER}/${id}`);
-    const json = await res.json();
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_TRANSFER}/${id}`);
+  const json = await res.json();
 
-    if (!res.ok) {
-      return notFound()
-    }
+  if (!res.ok) {
+    notFound();
+  }
 
-    const files = await Promise.all(
-      json.map(async (file: { url: string; name: string }, index: number) => {
-        const r = await fetch(file.url);
-        const blob = await r.blob();
-        return {
-          id: file.name + index,
-          blob: blob,
-          name: file.name,
-        };
-      })
-    );
-    return files;
-    // eslint-disable-next-line
+  const files = await Promise.all(
+    json.map(async (file: { url: string; name: string }, index: number) => {
+      const r = await fetch(file.url);
+      const blob = await r.blob();
+      return {
+        id: file.name + index,
+        blob: blob,
+        name: file.name,
+      };
+    })
+  );
+  return files;
+  // eslint-disable-next-line
 }
 
 export default async function Page({ params }: { params: { id: string } }) {
